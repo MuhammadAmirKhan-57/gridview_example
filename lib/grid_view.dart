0995ff0bb5b1fc347450675513b79443d29feda4
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-09 22:48:14 +0500

    GridView

--- lib/grid_view.dart
+++ lib/grid_view.dart
@@ -9,7 +9,55 @@ class GridViewScreen extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: Text('Grid View'),
+        backgroundColor: Colors.indigoAccent,
       ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        ),
+        children: [
+          Card(
+            elevation: 3,
+            margin: EdgeInsets.all(8.0),
+            color: Colors.teal.shade200,
+            child: Center(
+              child: Text("GridView"),
+            ),
+          ),
+           Card(
+            elevation: 3,
+            margin: EdgeInsets.all(8.0),
+            color: Colors.teal.shade200,
+            child: Center(
+              child: Text("GridView"),
+            ),
+          ),
+           Card(
+            elevation: 3,
+            margin: EdgeInsets.all(8.0),
+            color: Colors.blueGrey.shade200,
+            child: Center(
+              child: Text("GridView"),
+            ),
+          ),
+           Card(
+            elevation: 3,
+            margin: EdgeInsets.all(8.0),
+            color: Colors.amber.shade200,
+            child: Center(
+              child: Text("GridView"),
+            ),
+          ),
+           Card(
+            elevation: 3,
+            margin: EdgeInsets.all(8.0),
+            color: Colors.indigo.shade200,
+            child: Center(
+              child: Text("GridView"),
+            ),
+          ),
+        ],
+        ),
     );
   }
 }
